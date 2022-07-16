ca4b34f4facba13512477b1afcb737e5cf7c448d
Vitor Lira <mortedixs@gmail.com>
2022-07-16 15:13:34 -0300

    ultimo cambio pruebas

--- app_coder/views.py
+++ app_coder/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def inicio(request):
+
+    return render(request ,"app_coder/inicio.html")
+
+def cursos(request):
+
+    return render(request ,"app_coder/cursos.html")
+
+def profesores(request):
+
+    return render(request ,"app_coder/profesores.html")
+
+def estudiantes(request):
+
+    return render(request ,"app_coder/estudiantes.html")
+
+def entregables(request):
+
+    return render(request ,"app_coder/entregables.html")
